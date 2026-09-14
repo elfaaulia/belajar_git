1ac3e2e50170fbe38a60f039e8aa9f09fd8f449b
elfaaulia <celciaafle@gmail.com>
2026-09-14 09:15:00 +0700

    commit pertama

---
+++ sistem_bilangan/main.dart
@@ -0,0 +1,6 @@
+void main() {
+  print("Hello, World!");
+  print("Hello, saya Elfa Aulia Sahirah 👋");
+  print("Ini adalah program pertama saya menggunakan Dart 🚀");
+  print("Langkah kecil hari ini, menjadi programmer hebat di masa depan! 💻✨");
+}
